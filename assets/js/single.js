3d0cbd7ecb77fe0ffd8b51be2bf133e99082ceb0
Dalton Morrel <daltonmorrel7&@gmail.com>
2020-11-18 13:02:40 -0500

    can access GitHub issues and added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning")
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo)
 {
@@ -13,21 +15,50 @@ var getRepoIssues = function(repo)
             {
                 // pass data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link"))
+                {
+                    displayWarning(repo);
+                }
             });
         }
         else
         {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
 
+var getRepoName = function()
+{
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName)
+    {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    }
+    else
+    {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+    
+    
+};
+
 var displayIssues = function(issues)
 {
-    if (issues.length === 0) {
+    if (issues.length === 0) 
+    {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
-      }
+    }
 
     
 
@@ -66,4 +97,19 @@ var displayIssues = function(issues)
     }
 };
 
-getRepoIssues("Dmorrel7/git-it-done");
+var displayWarning = function(repo)
+{
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+
+getRepoName();
